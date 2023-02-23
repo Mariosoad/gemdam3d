73f09cf4116fbe1ab77e1a340440b8ee8a1666d2
Mariosoad <mario.bba@hotmail.com>
2023-02-23 04:16:35 -0300

    click on the model

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import React, { Suspense, useRef } from "react"
+import React, { Suspense, useState, useRef } from "react"
 import { Canvas } from "@react-three/fiber"
 import { Environment } from "@react-three/drei"
 import Model from "./Model"
 import Overlay from "./Overlay"
+import ModalInfo from "./ModalInfo"
 
 export default function App() {
   const overlay = useRef()
   const caption = useRef()
   const scroll = useRef(0)
+
+  const [showComponent, setShowComponent] = useState(false);
+
+  console.log(showComponent)
+
   return (
     <>
       <Canvas shadows eventSource={document.getElementById("root")} eventPrefix="client">
         <ambientLight intensity={1} />
         <Suspense fallback={null}>
-          <Model scroll={scroll} />
+          <Model scroll={scroll} setShowComponent={setShowComponent} />
           <Environment preset="city" />
         </Suspense>
       </Canvas>
-      <Overlay ref={overlay} caption={caption} scroll={scroll} />
+      
+      {showComponent == "Headphones" ?
+        <ModalInfo /> :
+        <Overlay ref={overlay} caption={caption} scroll={scroll} />
+      }
     </>
   )
 }
--- src/Overlay.js
+++ src/Overlay.js
@@ -48,8 +48,6 @@ function Formulario() {
     enviarMail(data)
   };
 
-  console.log(mensajeFormulario)
-
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)} className="container-form">
@@ -168,108 +166,109 @@ function Formulario() {
   )
 }
 
-
 const Overlay = forwardRef(({ caption, scroll }, ref) => (
-  <div
-    ref={ref}
-    onScroll={(e) => {
-      scroll.current = e.target.scrollTop / (e.target.scrollHeight - window.innerHeight)
-      caption.current.innerText = scroll.current.toFixed(2)
-    }}
+
+  <div ref={ref} onScroll={(e) => {
+    scroll.current = e.target.scrollTop / (e.target.scrollHeight - window.innerHeight)
+    caption.current.innerText = scroll.current.toFixed(2)
+  }}
     class="scroll">
-    <div id="gemdam" style={{ height: "400vh" }}>
-      <div class="dot">
-        <h1 className="tittle-gemdam"> GEMDAM </h1>
-        <br></br><br></br>
-        <p className="subtittle">Creamos soluciones útiles, creativas e innovadoras que resuelvan todo
-          tipo de problemas y se ajusten a tus requisitos.</p>
-      </div>
-    </div>
-    <div id="nosotros" style={{ height: "200vh" }}>
-      <div class="dot">
-        <h1>Nosotros</h1>
-        <br></br><br></br>
-        Somos una compañía dinámica y experta en la creación de experiencias digitales que busca transformar la comunicación estratégica dentro de las empresas
-        y en relación con sus clientes, para dar un salto de calidad en el ámbito cognitivo. Para esto desarrollamos propuestas capaces de mejorar los resultados
-        comerciales de cada marca y su relevancia, mediante un mejor posicionamiento en el mercado y un crecimiento de valor a lo largo de su ciclo de vida.
-        <ul>
-          <li>Valoramos la diversidad en un entorno abierto e inclusivo donde fomentamos la confianza y el trato respetuoso hacia cada individuo.</li><br></br>
-          {/* <li>Buscamos generar lazos de confianza a través de éticas sólidas y honestas, comunicándonos de manera clara y tomando responsabilidad por nuestras acciones.</li> */}
-          <li>Ayudamos a nuestros clientes a establecerse como grandes empresas de valor y relevancia, capaces de crear relaciones fructíferas a largo plazo.</li><br></br>
-          <li>Acompañamos cada proyecto desde su inicio, partiendo de etapas de asesoramiento y desarrollo, hasta su lanzamiento, estando presentes a cada momento para brindar respuestas eficaces.</li>
-        </ul>
-      </div>
-    </div>
-    <div id="team" style={{ height: "200vh" }}>
-      <div class="dot">
-        <h1> Team </h1>
-        <br></br><br></br>
 
-        <img className="foto-founder" width={100} height={100} src="https://res.cloudinary.com/deushkfkk/image/upload/v1674752608/GEMDAM/mario_vvix9l.png" />
-        <h3>Mario Hinostroza</h3>
-        CO-FOUNDER & CEO <br></br>
-        Programador, diseñador multimedia,
-        tester de videojuegos y un estratega
-        en la tecnología para mejorar la experiencia de vida
-
-        <br></br><br></br>
-
-        <img className="foto-founder" width={100} height={100} src="https://res.cloudinary.com/deushkfkk/image/upload/v1674752609/GEMDAM/rori_mzbthw.jpg" />
-        <h3>Rodrigo Isasmendi</h3>
-        CO-FOUNDER & ENVIRONMENT ARTIST <br></br>
-        
-      </div>
-    </div>
-    <div id="aumented" style={{ height: "200vh" }}>
-      <div class="dot">
-        <h1>Augmented Reality</h1>
-        <br></br><br></br>
-        La realidad aumentada tiene infinidad de aplicaciones con las que se innova día tras día y se logra llevar proyectos y empresas a un mayor desempeño.
-        La principal ventaja de la realidad aumentada yace en la facilidad que ésta otorga para el intercambio de información entre sus usuarios, ya sea entre la empresa y sus trabajadores,
-        o hacia los clientes, ya que permite la visualización de información en tiempo real sobre el mismo entorno, facilitando tareas, explicaciones y procesos de comunicación.
-        Además esta tecnología crea un nuevo canal de comunicación donde los clientes pueden interactuar en tiempo real con sus productos, servicios y experiencias que serán recordadas
-        con mayor facilidad por los clientes y crearán mayor conexión debido a su inmersividad y originalidad.
-      </div>
-    </div>
-    <div id="virtual" style={{ height: "200vh" }}>
-      <div class="dot">
-        <h1>Virtual Reality</h1>
-        <br></br><br></br>
-        La realidad virtual es un entorno digital inmersivo en el cual podemos sumergirnos a través de cascos o gafas de realidad virtual y la ayuda de otros dispositivos,
-        para vivir experiencias únicas, al igual que simulaciones de entornos y situaciones hasta donde llegue tu imaginación. Es un nuevo medio
-        para la creación de contenido y un nuevo canal de comunicación, por lo que es una opción nueva e ideal para la optimización de las comunicaciones tanto internas
-        como externas de una empresa,facilitando el desarrollo de actividades, además de reducir sus tiempos y costos.
-      </div>
-    </div>
-    <div id="desarrollos-web" style={{ height: "200vh" }}>
-      <div class="dot">
-        <h1>Desarrollo Web</h1>
-        <br></br><br></br>
-        Trabajamos codo a codo con nuestros clientes
-        para lograr la mejor estrategia de comunicación
-        acorde a las necesidades del negocio.
-        <br></br><br></br>
-        Realizamos programación a medida con el fin de
-        lograr propuestas en función de las necesidades
-        propuestas por nuestros clientes.
-        <br></br><br></br>
-        Realizamos webs autoadministrables ajustadas a la necesidad de cada cliente.
-        <br></br><br></br>
-        Contamos con servicio de diseño gráfico, para que nuestros clientes puedan desarrollar al máximo su negocio, logrando identidades potentes
-        para aumentar sus ventas.
-      </div>
-    </div>
-    <div id="conctacto" style={{ height: "200vh" }}>
-      <div class="dot">
-        <h1>Contacto</h1>
-        <br></br><br></br>
-        <Formulario />
-        <div className="copyright">
-          © 2023, GEMDAM STUDIO. Todos los derechos reservados.
+    <div>
+      <div id="gemdam" style={{ height: "400vh" }}>
+        <div class="dot">
+          <h1 className="tittle-gemdam"> GEMDAM </h1>
+          <br></br><br></br>
+          <p className="subtittle">Creamos soluciones útiles, creativas e innovadoras que resuelvan todo
+            tipo de problemas y se ajusten a tus requisitos.</p>
         </div>
       </div>
+      <div id="nosotros" style={{ height: "200vh" }}>
+        <div class="dot">
+          <h1>Nosotros</h1>
+          <br></br><br></br>
+          Somos una compañía dinámica y experta en la creación de experiencias digitales que busca transformar la comunicación estratégica dentro de las empresas
+          y en relación con sus clientes, para dar un salto de calidad en el ámbito cognitivo. Para esto desarrollamos propuestas capaces de mejorar los resultados
+          comerciales de cada marca y su relevancia, mediante un mejor posicionamiento en el mercado y un crecimiento de valor a lo largo de su ciclo de vida.
+          <ul>
+            <li>Valoramos la diversidad en un entorno abierto e inclusivo donde fomentamos la confianza y el trato respetuoso hacia cada individuo.</li><br></br>
+            {/* <li>Buscamos generar lazos de confianza a través de éticas sólidas y honestas, comunicándonos de manera clara y tomando responsabilidad por nuestras acciones.</li> */}
+            <li>Ayudamos a nuestros clientes a establecerse como grandes empresas de valor y relevancia, capaces de crear relaciones fructíferas a largo plazo.</li><br></br>
+            <li>Acompañamos cada proyecto desde su inicio, partiendo de etapas de asesoramiento y desarrollo, hasta su lanzamiento, estando presentes a cada momento para brindar respuestas eficaces.</li>
+          </ul>
+        </div>
+      </div>
+      <div id="team" style={{ height: "200vh" }}>
+        <div class="dot">
+          <h1> Team </h1>
+          <br></br><br></br>
 
+          <img className="foto-founder" width={100} height={100} src="https://res.cloudinary.com/deushkfkk/image/upload/v1674752608/GEMDAM/mario_vvix9l.png" />
+          <h3>Mario Hinostroza</h3>
+          CO-FOUNDER & CEO <br></br>
+          Programador, diseñador multimedia,
+          tester de videojuegos y un estratega
+          en la tecnología para mejorar la experiencia de vida.
+
+          <br></br><br></br>
+
+          <img className="foto-founder" width={100} height={100} src="https://res.cloudinary.com/deushkfkk/image/upload/v1674752609/GEMDAM/rori_mzbthw.jpg" />
+          <h3>Rodrigo Isasmendi</h3>
+          CO-FOUNDER & ENVIRONMENT ARTIST <br></br>
+          Diseñador Multimedia, Environment Artist y diseñador de experiencias.
+        </div>
+      </div>
+      <div id="aumented" style={{ height: "200vh" }}>
+        <div class="dot">
+          <h1>Augmented Reality</h1>
+          <br></br><br></br>
+          La realidad aumentada tiene infinidad de aplicaciones con las que se innova día tras día y se logra llevar proyectos y empresas a un mayor desempeño.
+          La principal ventaja de la realidad aumentada yace en la facilidad que ésta otorga para el intercambio de información entre sus usuarios, ya sea entre la empresa y sus trabajadores,
+          o hacia los clientes, ya que permite la visualización de información en tiempo real sobre el mismo entorno, facilitando tareas, explicaciones y procesos de comunicación.
+          Además esta tecnología crea un nuevo canal de comunicación donde los clientes pueden interactuar en tiempo real con sus productos, servicios y experiencias que serán recordadas
+          con mayor facilidad por los clientes y crearán mayor conexión debido a su inmersividad y originalidad.
+        </div>
+      </div>
+      <div id="virtual" style={{ height: "200vh" }}>
+        <div class="dot">
+          <h1>Virtual Reality</h1>
+          <br></br><br></br>
+          La realidad virtual es un entorno digital inmersivo en el cual podemos sumergirnos a través de cascos o gafas de realidad virtual y la ayuda de otros dispositivos,
+          para vivir experiencias únicas, al igual que simulaciones de entornos y situaciones hasta donde llegue tu imaginación. Es un nuevo medio
+          para la creación de contenido y un nuevo canal de comunicación, por lo que es una opción nueva e ideal para la optimización de las comunicaciones tanto internas
+          como externas de una empresa,facilitando el desarrollo de actividades, además de reducir sus tiempos y costos.
+        </div>
+      </div>
+      <div id="desarrollos-web" style={{ height: "200vh" }}>
+        <div class="dot">
+          <h1>Desarrollo Web</h1>
+          <br></br><br></br>
+          Trabajamos codo a codo con nuestros clientes
+          para lograr la mejor estrategia de comunicación
+          acorde a las necesidades del negocio.
+          <br></br><br></br>
+          Realizamos programación a medida con el fin de
+          lograr propuestas en función de las necesidades
+          propuestas por nuestros clientes.
+          <br></br><br></br>
+          Realizamos webs autoadministrables ajustadas a la necesidad de cada cliente.
+          <br></br><br></br>
+          Contamos con servicio de diseño gráfico, para que nuestros clientes puedan desarrollar al máximo su negocio, logrando identidades potentes
+          para aumentar sus ventas.
+        </div>
+      </div>
+      <div id="conctacto" style={{ height: "200vh" }}>
+        <div class="dot">
+          <h1>Contacto</h1>
+          <br></br><br></br>
+          <Formulario />
+          <div className="copyright">
+            © 2023, GEMDAM STUDIO. Todos los derechos reservados.
+          </div>
+        </div>
+      </div>
     </div>
+
     {/* <span class="caption" ref={caption}>
       0.00
     </span> */}
